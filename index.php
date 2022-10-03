baf57e0ef2f17e683be75262506c38bdfc8a3c64
Anuska86 <anasrm86@gmail.com>
2022-10-03 18:02:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <html>
 
+<head>
+    <meta charset="UTF-8" />
+</head>
+
 <body>
     <?php
 
-    echo "<h1>ACTIVIDAD 1.1</h1>";
+    echo     "<h1>Desarrollo web en entorno servidor: Actividad 1. Ana Sappia Rey. </h1>";
+
+    echo "<h2>Actividad 1.1</h2>";
 
     /* 1.1.	Escribir un programa en que se declare una variable que contenga un valor numérico (el que tú quieras). 
     Este valor representará una cantidad en euros. El programa calculará el equivalente a otras dos monedas: dólares americanos y yenes. 
@@ -11,8 +17,9 @@
     XX euros equivalen a YY dólares y ZZ yenes.
     */
 
-    const euro_dollar = 0.98;
-    const euro_yen = 141.89;
+    define("euro_dollar", 0.98);
+    define("euro_yen", 141.89);
+
 
     $euros = 10;
     $dollars = $euros * euro_dollar;
@@ -20,12 +27,12 @@
 
     echo "$euros euros equivalen a $dollars dólares y $yens yenes";
 
-    echo "<h1>ACTIVIDAD 1.2</h1>";
+    echo "<h2>Actividad 1.2</h2>";
 
     /*1.2.	En una escuela tenemos tres clases y queremos saber la cantidad de pupitres que necesitamos tener en total. 
     Dependerá del número de alumnos por aula. Hay que tener en cuenta que en cada pupitre caben 2 alumnos. 
     Haz un programa en PHP en que se definan tres variables que contengan el número de alumnos que hay en cada clase, 
-    y muestre el número total de pupitres que necesitaremos. */ 
+    y muestre el número total de pupitres que necesitaremos. */
 
     $Aclass = 16;
     $Bclass = 21;
@@ -38,11 +45,11 @@
     echo "Necesitaremos un total de $totalDesks pupitres";
 
 
-    echo "<h1>ACTIVIDAD 1.3</h1>";
+    echo "<h2>Actividad 1.3</h2>";
 
     /*1.3.	Escribir un programa que guarde en tres variables los coeficientes de una ecuación de segundo grado. 
     Debe guardar en otras dos variables los resultados, y mostrarlos por pantalla. 
-    Para hacer la raíz cuadrada, usar la función de PHP sqrt (número). */ 
+    Para hacer la raíz cuadrada, usar la función de PHP sqrt (número). */
 
     $a = 3;
     $b = 8;
@@ -56,10 +63,10 @@
     echo "El resultado de la segunda ecuación es " . sprintf("%.2f", $ec2) . "<br/>";
 
 
-    echo "<h1>ACTIVIDAD 1.4</h1>";
+    echo "<h2>Actividad 1.4</h2>";
 
     /*1.4.	Escribir un programa que, dado el diámetro de un a pizza, nos muestre su área y la longitud del borde. 
-    Los resultados deben mostrarse con un máximo de 2 decimales.*/ 
+    Los resultados deben mostrarse con un máximo de 2 decimales.*/
 
     $pizzaDiam = 5;
     $pizzaRadio = $pizzaDiam / 2;
@@ -68,16 +75,15 @@
 
     echo "El área de la pizza es de" . sprintf("%.2f", $pizzaArea) . "cm2 y la longitud de" . sprintf("%.2f", $pizzaLarge) . " centímetros";
 
-    echo "<h1>ACTIVIDAD 1.5</h1>";
+    echo "<h2>Actividad 1.5</h2>";
 
     /*1.5.	A un trabajador le pagan por horas a razón de 30€/h. A partir de las 40 horas de trabajo, 
     la tarifa por hora se incrementa en un 50%. Escribe un programa en que, a partir de un número de horas trabajadas, 
-    se muestre como resultado el sueldo del trabajador. Los valores del precio por hora y el incremento deben almacenarse en constantes. */ 
+    se muestre como resultado el sueldo del trabajador. Los valores del precio por hora y el incremento deben almacenarse en constantes. */
 
-    const hourPrice = 30;
+    define("hourPrice", 30);
 
-    const extraHourPrice = 45;
-
+    define("extraHourPrice", 45);
 
     $totalHours = 42;
     $totalExtraHours = 0;
@@ -92,7 +98,6 @@
     } else {
         $salary = $totalHours * hourPrice;
     }
-
 
     echo "El total del salario es de $salary euros. ";
 
